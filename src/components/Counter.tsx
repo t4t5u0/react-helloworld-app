187671cc55dcacef3111aec1a133f8b3d5748ff6
t4t5u0 <ymmtryk0902@gmail.com>
2021-07-04 22:20:26 +0900

    [fix] JSX.Element[] を返す必要があった

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,81 +1,12 @@
 import React, { useState } from 'react';
 import './Counter.scss';
 
-const Counter: React.VFC = () => {
-  const [count, setCount] = useState(0);
-
-  // type CounterClass = {
-  //   character: string;
-  //   function: (count: number) => number;
-  //   className: string;
-  // };
-  // type CountersClass = CounterClass[];
-
-  // const containers: CountersClass = [
-  //   {
-  //     character: '+',
-  //     function: incrementCounter,
-  //     className: 'Counter-button-inc',
-  //   },
-  //   {
-  //     character: '-',
-  //     function: decrementCounter,
-  //     className: 'Counter-button-dec',
-  //   },
-  //   {
-  //     character: 'reset',
-  //     function: resetCounter,
-  //     className: 'Counter-button-res',
-  //   },
-  // ];
-
-  // const Counters = (containers: CountersClass) => {
-  //   const list = containers.map((container: CounterClass) => {
-  //     return (
-  //       <div className="Counter">
-  //         <button
-  //           className={`Counter-button ${container.className}`}
-  //           onClick={() => setCount(container.function(count))}>
-  //           {container.character}
-  //         </button>
-  //       </div>
-  //     );
-  //   });
-  //   return list;
-  // };
-
-  return (
-    <div>
-      <p className="Counter-title">You clicked {count} times</p>
-      <div className="Counters">
-        {/* {Counters} */}
-        <div className="Counter">
-          <button
-            className={`Counter-button Counter-button-inc`}
-            onClick={() => setCount(incrementCounter(count))}>
-            +
-          </button>
-        </div>
-        <div className="Counter">
-          <button
-            className={`Counter-button Counter-button-dec`}
-            onClick={() => setCount(decrementCounter(count))}>
-            -
-          </button>
-        </div>
-        <div className="Counter">
-          <button
-            className={`Counter-button Counter-button-res`}
-            onClick={() => setCount(resetCounter(count))}>
-            reset
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+type CounterClass = {
+  character: string;
+  function: (count: number) => number;
+  className: string;
 };
-
-export default Counter;
+type CountersClass = CounterClass[];
 
 const incrementCounter = (count: number) => {
   return count + 1;
@@ -88,3 +19,46 @@ const decrementCounter = (count: number) => {
 const resetCounter = (_: number) => {
   return 0;
 };
+
+const containers: CountersClass = [
+  {
+    character: '+',
+    function: incrementCounter,
+    className: 'Counter-button-inc',
+  },
+  {
+    character: '-',
+    function: decrementCounter,
+    className: 'Counter-button-dec',
+  },
+  {
+    character: 'reset',
+    function: resetCounter,
+    className: 'Counter-button-res',
+  },
+];
+
+const Counter: React.VFC = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p className="Counter-title">You clicked {count} times</p>
+      <div className="Counters">
+        {containers.map((container) => {
+          return (
+            <div className="Counter" key={container.character}>
+              <button
+                className={`Counter-button ${container.className}`}
+                onClick={() => setCount(container.function(count))}>
+                {container.character}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
